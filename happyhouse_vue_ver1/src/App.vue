33e4fd5a39cba490c3387c5660f033ae6d7a7453
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-05-15 22:06:58 +0900

    scr init

---
+++ happyhouse_vue_ver1/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <HeaderView></HeaderView>
+    <router-view></router-view>
+    <FooterView></FooterView>
+  </div>
+</template>
+
+<script>
+import HeaderView from "@/components/common/HeaderView.vue";
+import FooterView from "@/components/common/FooterView.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderView,
+    FooterView,
+  },
+};
+</script>
+
+<style>
+a {
+  text-decoration: none !important;
+}
+</style>
