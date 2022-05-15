4801caf0e3d9582664870c924c6bd898c00772aa
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-05-15 22:07:39 +0900

    Create NoticeView.vue

---
+++ happyhouse_vue_ver1/src/views/NoticeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- ======= 페이지 제목 섹션 Start ======= -->
+    <section class="PageTitle">
+      <div class="container">
+        <div
+          class="section-title pt-3 pb-0 d-flex justify-content-between align-items-center"
+        >
+          <h2>공지사항</h2>
+          <ol>
+            <li><a href="/">Home</a></li>
+            <li>공지사항</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+    <!-- 페이지 제목 섹션 End -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
