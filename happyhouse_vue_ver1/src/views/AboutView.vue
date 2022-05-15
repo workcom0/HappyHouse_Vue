527890067fd2a1929667ba51d5d996124695a469
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-05-15 22:07:30 +0900

    Create AboutView.vue

---
+++ happyhouse_vue_ver1/src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- ======= 페이지 제목 섹션 Start ======= -->
+    <section class="PageTitle">
+      <div class="container">
+        <div
+          class="section-title pt-3 pb-0 d-flex justify-content-between align-items-center"
+        >
+          <h2>About</h2>
+          <ol>
+            <li><a href="/">Home</a></li>
+            <li>About</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+    <!-- 페이지 제목 섹션 End -->
+
+    <!-- ======= 소개 Start ======= -->
+    <section id="pages-details" class="pages-details">
+      <div class="container">
+        <div class="col-lg-12" align="center" style="margin: auto">
+          <div class="card-body">
+            <h1>AboutView 화면입니다.</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- 소개 End -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
