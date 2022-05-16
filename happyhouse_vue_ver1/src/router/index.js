76ddf5297b5fbbe54c5ac8842752963d1b46ff19
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-05-16 19:48:41 +0900

    Update index.js

--- happyhouse_vue_ver1/src/router/index.js
+++ happyhouse_vue_ver1/src/router/index.js
@@ -3,6 +3,11 @@ import VueRouter from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import NoticeView from "@/views/NoticeView.vue";
+import LoginView from "@/views/LoginView.vue";
+import JoinView from "@/views/JoinView.vue";
+import NoticeList from "@/components/notice/NoticeList";
+import NoticeWrite from "@/components/notice/NoticeWrite";
+import NoticeDetail from "@/components/notice/NoticeDetail";
 
 Vue.use(VueRouter);
 
@@ -19,8 +24,34 @@ const routes = [
   },
   {
     path: "/notice",
-    name: "notice",
     component: NoticeView,
+    children: [
+      {
+        path: "",
+        name: "notice-list",
+        component: NoticeList,
+      },
+      {
+        path: "write",
+        name: "notice-write",
+        component: NoticeWrite,
+      },
+      {
+        path: "detail",
+        name: "notice-detail",
+        component: NoticeDetail,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/join",
+    name: "join",
+    component: JoinView,
   },
 ];
 
