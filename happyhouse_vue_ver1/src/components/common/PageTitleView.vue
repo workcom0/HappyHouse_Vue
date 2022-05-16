8522f7daff3f32361d98aa0af9265fbf08b6bac1
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-05-16 19:48:35 +0900

    Create PageTitleView.vue

---
+++ happyhouse_vue_ver1/src/components/common/PageTitleView.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- ======= 페이지 제목 섹션 Start ======= -->
+  <section class="PageTitle">
+    <div class="container">
+      <div
+        class="section-title pt-3 pb-0 d-flex justify-content-between align-items-center"
+      >
+        <h2>{{ title }}</h2>
+        <ol>
+          <li><a href="${root}/">Home</a></li>
+          <li>{{ title }}</li>
+        </ol>
+      </div>
+    </div>
+  </section>
+  <!-- 페이지 제목 섹션 End -->
+</template>
+
+<script>
+export default {
+  name: "PageTitleView",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style></style>
